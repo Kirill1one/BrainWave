7e424a5a15c0a195a8bc8c5fa603a754bb3509d1
Kirill1one <kirill2002ku@gmail.com>
2024-07-17 11:42:27 +0300

    Bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -11,6 +16,11 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
 
       <ButtonGradient />
